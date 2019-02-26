18ce5b030b3303ea00abb56c0623b978d175c942
JizongL <2015rpro@gmail.com>
2019-02-26 00:53:01 -0500

    further worked on the js file, loop is broken needed to be fix david

---
+++ QuestionAnswer.js
@@ -0,0 +1,72 @@
+https://www.petful.com/misc/dog-trivia-questions/
+
+1. Normal adult dogs have how many teeth?
+
+A) 24
+B) 38
+C) 42
+D) 32
+
+2. Through what part of the body do dogs sweat?
+
+A) Mouth
+B) Ears
+C) Nose
+D) Paws
+
+3. True or False: Dogs can only see in black and white.
+
+4. What is the most common training command taught to dogs?
+
+A) Stay
+B) Beg
+C) Sit
+D) Dance
+
+5. What is a dog’s most highly developed sense?
+
+A) Taste
+B) Smell
+C) Sight
+D) Touch
+
+6. Puppies are delivered how many weeks after conception?
+
+A) 36
+B) 22
+C) 9
+D) 16
+
+7. What is the favorite dog breed of the Queen of England?
+
+A) Corgi
+B) Basenji
+C) Poodle
+D) Pomeranian
+
+8. Which TV series had a dog named K9 who was also a robot?
+A) Full House
+B) Star Trek
+C) Doctor Who
+D) Law & Order
+
+9. Which dog breed is the smallest of them all?
+
+A) Dachshund
+B) Shih tzu
+C) Pomeranian
+D) Chihuahua
+
+10. Which breed was once known as St. John’s Newfoundland?
+
+A) Newfoundland
+B) Golden retriever
+C) Labrador
+D) Puli
+
+11. Which dog breed has a black tongue?
+
+A) Husky
+B) Labrador
+C) Weimaraner
+D) Chow chow
--- quiz.js
+++ quiz.js
@@ -1,43 +1,61 @@
 'use strict';
 
 const questionOne = '1. Normal adult dogs have how many teeth?';
-const keyOne = {a:24,b:38,c:43,d:32};
+const keyOne = {a:24,b:38,c:42,d:32};
+const explainationOne = 'this is a test explain for question 2';
 const questionTwo = '2. Through what part of the body do dogs sweat?';
 const keyTwo = {a:'Mouth',b:'Ears',c:'Nose',d:'Paws'};
-
+const explainationTwo = 'this is a test explain for question 2';
 const QUIZBASE = {
   QUIZ:[
-    {id:1,question:questionOne,answerKey:keyOne,completed:false},
-    {id:2,question:questionTwo,answerKey:keyTwo,completed:false}
+    {id:1,question:questionOne,answerKey:keyOne,correctKey:'c',completed:false,submittedKey:'',explaination:explainationOne},
+    {id:2,question:questionTwo,answerKey:keyTwo,correctKey:'d',completed:false,submittedKey:'',explaination:explainationTwo}
   ],
   quizStart:false,
   score:0,
-  
 };
 
 
 
 
-function createQuiz(){
+function createQuiz(n){
   console.log('`createQuz ran`');
-  console.log('click quiz',QUIZBASE.QUIZ[0].question);
+  console.log('click quiz',QUIZBASE.QUIZ[n].question);
 
-  const quizElementString = `<p>${QUIZBASE.QUIZ[0].question}</p>
-  <ul>
-    <li>${QUIZBASE.QUIZ[0].answerKey.a}</li>
-    <li>${QUIZBASE.QUIZ[0].answerKey.b}</li>
-    <li>${QUIZBASE.QUIZ[0].answerKey.c}</li>
-    <li>${QUIZBASE.QUIZ[0].answerKey.d}</li>
+  const quizElementString = `<div class='quiz-question'>${QUIZBASE.QUIZ[n].question}</div>
+  <ul class='quiz-answer-field'>
+    <li id='a'>A: ${QUIZBASE.QUIZ[n].answerKey.a}</li>
+    <li id='b'>B: ${QUIZBASE.QUIZ[n].answerKey.b}</li>
+    <li id='c'>C: ${QUIZBASE.QUIZ[n].answerKey.c}</li>
+    <li id='d'>D: ${QUIZBASE.QUIZ[n].answerKey.d}</li>
   </ul>
   <button id = 'quiz-next' type = 'submit'>Next</button>
   `;
   $('.quiz-area').html(quizElementString);
 }
 
+
+
 function render(){
   if(QUIZBASE.quizStart){
-    createQuiz();
-  }
+    createQuiz(0);
+    //let quizNumber = 0;
+    // while(quizNumber < 10){
+    //   createQuiz(quizNumber);
+      // while(!QUIZBASE.QUIZ[quizNumber].completed)
+      // {console.log('testing whileloop');
+      //   $('.quiz-answer-field').on('click','li',function(event){
+      //     const answerChosen = $(this).attr('id');
+      //     QUIZBASE.QUIZ[quizNumber].completed = true;  
+
+          
+        //});
+      //   quizNumber+=1;
+      // }
+      
+    }
+    
+  
   console.log('`render ran`');
 }
 
